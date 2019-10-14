c05551b143d95b3e405b6a66ed6e0184ff86f50d
el-tocino <email+github@aforkinthetoad.com>
2019-10-14 18:23:59 -0500

    Update Hallow.py

--- Hallow.py
+++ Hallow.py
@@ -1,7 +1,8 @@
+import time
+import array
 from gpiozero import Button
 from picamera import PiCamera
-from DmxPy import DmxPy
-import time
+from ola.ClientWrapper import ClientWrapper
 
 # set gpio stuff
 button = Button(21) # pin 40 on a pi 3b.
@@ -12,51 +13,7 @@ cam.exposure_mode = 'off'
 cam.awb_mode = 'off'
 cam.iso = 200
 cam.sensor_mode = 2
-### determine shutter speed for pics and set here
-### cam.shutter_speed = int(ss)
-
-# set dmx stuff
-try:
-    dmx = DmxPy('/dev/ttyUSB0')
-except:
-    dmx = DmxPy('/dev/ttyUSB1')
-elsE:
-    print ("failed to open dmx device!")
-    sys.exit()      
-    
-# RUN FOREVER!
-while True:
-    if button.is_pressed:
-        fn = str(time.time()) + ".png"
-        dmx.setChannel(0,200)
-        dmx.setChannel(1,200)
-        dmx.setChannel(2,200)
-        dmx.setChannel(3,200)
-        dmx.render()
-        cam.capture(fn)
-        dmx.blackout()
-        dmx.render()
-        time.sleep(3)
-    else:
-        time.sleep(.1)
-
-    
-
-
-
-
-
-    
-    
-    
-    
-    
-import array
-import time
-from picamera import PiCamera
-from ola.ClientWrapper import ClientWrapper
-
-
+cam.shutter_speed = int(67998)
 
 def DmxSent(state):
   wrapper.Stop()
@@ -67,21 +24,16 @@ lgreen = array.array('B', [100, 0, 100])
 lblue = array.array('B', [100, 0, 0, 100])
 lall = array.array('B', [250, 220, 200, 220])
 loff = array.array('B', [0, 0, 0, 0, 0])
-
-
-# set cam stuff
-cam = PiCamera()
-cam.exposure_mode = 'off'
-cam.awb_mode = 'off'
-cam.iso = 200
-cam.sensor_mode = 2
-cam.shutter_speed = int(67998)
-
-fn = str(time.time()) + ".png"
-wrapper = ClientWrapper()
-client = wrapper.Client()
-client.SendDmx(universe, lall, DmxSent)
-cam.capture(fn)
-client.SendDmx(universe, loff, DmxSent)
-
     
+# RUN FOREVER!
+while True:
+    if button.is_pressed:
+        fn = str(time.time()) + ".png"
+        wrapper = ClientWrapper()
+        client = wrapper.Client()
+        client.SendDmx(universe, lall, DmxSent)
+        cam.capture(fn)
+        client.SendDmx(universe, loff, DmxSent)
+        time.sleep(3)
+    else:
+        time.sleep(.1)
