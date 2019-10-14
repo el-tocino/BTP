1e701cd956856b407382a87e4c508f28c70e5ed2
el-tocino <email+github@aforkinthetoad.com>
2019-10-13 22:04:02 -0500

    Update Hallow.py

--- Hallow.py
+++ Hallow.py
@@ -45,3 +45,43 @@ while True:
 
 
 
+
+    
+    
+    
+    
+    
+import array
+import time
+from picamera import PiCamera
+from ola.ClientWrapper import ClientWrapper
+
+
+
+def DmxSent(state):
+  wrapper.Stop()
+
+universe = 1
+lred = array.array('B', [100, 100])
+lgreen = array.array('B', [100, 0, 100])
+lblue = array.array('B', [100, 0, 0, 100])
+lall = array.array('B', [250, 220, 200, 220])
+loff = array.array('B', [0, 0, 0, 0, 0])
+
+
+# set cam stuff
+cam = PiCamera()
+cam.exposure_mode = 'off'
+cam.awb_mode = 'off'
+cam.iso = 200
+cam.sensor_mode = 2
+cam.shutter_speed = int(67998)
+
+fn = str(time.time()) + ".png"
+wrapper = ClientWrapper()
+client = wrapper.Client()
+client.SendDmx(universe, lall, DmxSent)
+cam.capture(fn)
+client.SendDmx(universe, loff, DmxSent)
+
+    
